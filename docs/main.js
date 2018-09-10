73e1765f23fa421517dc45d27bc2cb0986254514
Kazunee <33825336+Kazunee@users.noreply.github.com>
2018-09-10 20:44:54 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -17,7 +17,6 @@ function redraw() {
     img.onload = function() {
         ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
         ctx.font = "100px 'ＭＳ ゴシック'";
-        fld1.value = canvas.toDataURL();
     };
     img.src = "https://s3-ap-northeast-1.amazonaws.com/alserver.jp/media_attachments/files/000/068/987/original/3dfdf98d851f4dfe.png";
 }
