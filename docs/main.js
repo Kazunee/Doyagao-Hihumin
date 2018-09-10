344ab4eba9c5bf3c39403540d7807bd056e73e6f
Kazunee <33825336+Kazunee@users.noreply.github.com>
2018-09-10 22:46:32 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -4,13 +4,20 @@ window.onload = function () {
   canvas = document.getElementById("canvas");
   textbox = document.getElementById("textbox");
   ctx = canvas.getContext('2d');
-  ctx.lineJoin = 'round';
   var img = new Image();
     img.onload = function() {
         ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
     };
     img.src = "https://s3-ap-northeast-1.amazonaws.com/alserver.jp/media_attachments/files/000/068/562/original/46f0eaca010dee0c.png";
 };
+
+function redraw() {
+    ctx.fillRect(0, 460, canvas.width, 60);
+    ctx.textAlign="center"
+    ctx.fillStyle = "black"
+    ctx.font = "50px UD Digi Kyokasho NP-B";
+    ctx.fillText(textbox, canvas.width, canvas.height);
+}
 
 function saveImage() {
   var a = document.createElement("a");
@@ -21,12 +28,3 @@ function saveImage() {
   document.body.removeChild(a);
 }
 
-function redraw() {
-    var str = document.info.message.value;
-    ctx.fillStyle = "blue";
-    ctx.fillRect(0, 460, canvas.width, 60);
-    ctx.textAlign="center"
-    ctx.fillStyle = "black"
-    ctx.font = "50px UD Digi Kyokasho NP-B";
-    ctx.fillText(str, canvas.width, canvas.height);
-}
