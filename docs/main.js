833f9b10b8dd6f8781f1afc8c4a3ca357bc3a255
Kazunee <33825336+Kazunee@users.noreply.github.com>
2018-09-10 19:25:06 +0900

    Create main.js

---
+++ docs/main.js
@@ -0,0 +1,18 @@
+var canvas, ctx , textbox;
+
+window.onload = function () {
+  canvas = document.getElementById("canvas");
+  textbox = document.getElementById("textbox");
+  ctx = canvas.getContext('2d');
+  ctx.font = '100px UDデジタル教科書体NP-B';
+  ctx.lineJoin = 'round';
+};
+
+function saveImage() {
+  var a = document.createElement("a");
+  a.href = canvas.toDataURL("image/png");
+  a.setAttribute("download", "image.png");
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+};
