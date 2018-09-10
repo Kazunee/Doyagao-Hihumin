2fec7b3485b78de9535f6301b6ea910204dff439
Kazunee <33825336+Kazunee@users.noreply.github.com>
2018-09-10 20:42:26 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,16 +1,5 @@
 var canvas, ctx , textbox;
 
-window.onload = function () {
-  canvas = document.getElementById("canvas");
-  textbox = document.getElementById("textbox");
-  ctx = canvas.getContext('2d');
-  var img = new Image();
-  img.src = "https://s3-ap-northeast-1.amazonaws.com/alserver.jp/media_attachments/files/000/068/987/original/3dfdf98d851f4dfe.png";
-  ctx.drawImage(img, 0, 0);
-  ctx.font = '100px serif';
-  ctx.lineJoin = 'round';
-};
-
 function saveImage() {
   var a = document.createElement("a");
   a.href = canvas.toDataURL("image/png");
@@ -21,10 +10,14 @@ function saveImage() {
 }
 
 function redraw() {
-  ctx.setTransform(1,0,0,1,0,0);
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.setTransform(1,0,-0.4,1,0,0);
-  var posx = 70;
-  var posy = 100;
-  var text = textbox.value;
+  canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    /* Imageオブジェクトを生成 */
+    var img = new Image();
+    img.onload = function() {
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+        ctx.font = "100px 'ＭＳ ゴシック'";
+        fld1.value = canvas.toDataURL();
+    };
+    img.src = "https://s3-ap-northeast-1.amazonaws.com/alserver.jp/media_attachments/files/000/068/987/original/3dfdf98d851f4dfe.png";
 }
