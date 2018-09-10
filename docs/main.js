44360f5b1a3afd2d7eb381c474ba892f19ea1ef3
Kazunee <33825336+Kazunee@users.noreply.github.com>
2018-09-10 22:23:44 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -4,18 +4,10 @@ window.onload = function () {
   canvas = document.getElementById("canvas");
   textbox = document.getElementById("textbox");
   ctx = canvas.getContext('2d');
-  ctx.font = '140px serif';
   ctx.lineJoin = 'round';
   var img = new Image();
     img.onload = function() {
         ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-        ctx.fillStyle = "blue";
-        ctx.fillRect(0, 460, canvas.width, 60);
-        ctx.textAlign="center";
-        ctx.fillStyle = "white";
-        ctx.font = "30px 'ＭＳ ゴシック'";
-        ctx.fillText("見出しテキスト", canvas.width/2, canvas.height);
-        fld1.value = canvas.toDataURL();
     };
     img.src = "https://s3-ap-northeast-1.amazonaws.com/alserver.jp/media_attachments/files/000/068/987/original/3dfdf98d851f4dfe.png";
 };
@@ -31,4 +23,10 @@ function saveImage() {
 
 function redraw() {
     var str = document.info.message.value;
+    ctx.fillStyle = "blue";
+    ctx.fillRect(0, 460, canvas.width, 60);
+    ctx.textAlign="right
+    ctx.fillStyle = "black
+    ctx.font = "140px UD Digi Kyokasho NP-B";
+    ctx.fillText(str, canvas.width/2, canvas.height);
 }
