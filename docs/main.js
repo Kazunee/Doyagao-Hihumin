75acc8ee0e5f768a53028823dd93b1ca06bab3fa
Kazunee <33825336+Kazunee@users.noreply.github.com>
2018-09-10 22:07:56 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -13,11 +13,5 @@ function redraw() {
   canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
     var str = document.info.message.value;
-    /* Imageオブジェクトを生成 */
-    var img = new Image();
-    img.onload = function() {
-        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-        ctx.font = "100px 'ＭＳ ゴシック'";
-    };
-    img.src = "https://s3-ap-northeast-1.amazonaws.com/alserver.jp/media_attachments/files/000/068/987/original/3dfdf98d851f4dfe.png";
+    alert(str);
 }
